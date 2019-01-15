c6cd98b74197ff655ecb8d9765020dbb0bc35f75
kinga00099 <kinga00099@gmail.com>
2019-01-15 15:01:13 +0100

    project page prepared

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ProjectPage msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProjectPage from './components/ProjectPage.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+      ProjectPage
   }
 }
 </script>
---
+++ src/components/ProjectPage.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-4">
+                <img class="logo" alt="Vue logo" src="../assets/logos/logo1.png">
+                <p>Status: open</p>
+                <p>Tags: #healthcare</p>
+            </div>
+
+            <div class="col-md-8">
+                <h1>Project neve</h1>
+                <div class="row">
+                    <div class="col-md-4">
+                        <p>Organisation</p>
+                    </div>
+                    <div class="col-md-8">
+                        <p> Vöröskereszt</p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <p>Description</p>
+                    </div>
+                    <div class="col-md-8">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo
+                            sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo
+                            sagittis,
+                            sapien dui mattis dui, non pulvinar lorem felis nec erat
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo
+                            sagittis,
+                            sapien dui
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo
+                            sagittis,
+                            sapien dui mattis dui, non pulvinar lorem felis nec eratmattis dui, non pulvinar lorem felis
+                            nec
+                            erat</p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <p>Description</p>
+                    </div>
+                    <div class="col-md-8">
+                        <p>Requirements: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut
+                            commodo
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo
+                            sagittis,
+                            sapien dui mattis dui, non pulvinar lorem felis nec erat
+                            sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo
+                            sagittis,
+                            sapien dui mattis dui, non pulvinar lorem felis nec erat
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo
+                            sagittis,
+                            sapien dui mattis dui, non pulvinar lorem felis nec erat
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo
+                            sagittis,
+                            sapien dui mattis dui, non pulvinar lorem felis nec erat
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo
+                            sagittis,
+                            sapien dui mattis dui, non pulvinar lorem felis nec erat</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+
+    .logo {
+        width: 150px;
+        height: 150px;
+    }
+</style>
