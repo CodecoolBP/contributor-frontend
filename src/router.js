52f7c91ce82bc8a862b9eaf036efca219dbade74
kinga00099 <kinga00099@gmail.com>
2019-01-16 22:34:17 +0100

    home route added to vue router

--- src/router.js
+++ src/router.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import MainPage from './components/MainPage.vue'
 import ProjectPage from './components/ProjectPage.vue'
 
 Vue.use(Router);
 
 export default new Router({
     routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: MainPage,
+        },
         {
             path: '/projects/',
             name: 'projects',
