7672924ecce0a21fcf08b76bb7f3c702717ae9ea
kinga00099 <kinga00099@gmail.com>
2019-01-17 23:07:59 +0100

    navbar renewed, some design elements added

--- src/components/ProjectPage.vue
+++ src/components/ProjectPage.vue
@@ -2,92 +2,103 @@
     <body>
     <div class="container-fluid">
 
-        <nav class="navbar navbar-expand-lg  bg-dark">
-            <a class="navbar-brand" href="#">Navbar</a>
-
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="navbar-collapse collapse w-100 dual-collapse2 order-1 order-md-0">
+                <ul class="navbar-nav ml-auto text-center">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="/">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="#">Success stories</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
+                        <a class="nav-link" href="#">TOP lists</a>
                     </li>
                 </ul>
+            </div>
 
+            <div class="navbar-collapse collapse w-100 dual-collapse2 order-2 order-md-2">
+                <ul class="navbar-nav mr-auto text-center">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Add new project</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
             </div>
         </nav>
 
     </div>
     <div class="container-fluid">
-    <div class="container">
+        <div class="container">
 
-        <div v-if="projects && projects.length">
-            <div v-for="project of projects">
-                <div class="row">
+            <div class="bigcard" v-if="projects && projects.length">
+                <div v-for="project of projects">
+                    <div class="row">
 
-                    <div class="col-md-4">
-                        <img class="logo" alt="Vue logo" src="../assets/img/logos/logo1.png">
-                        <br><br>
-                        <p>Status: {{project.status}}</p>
-                        <p>Tags:
-                            <span v-for="tag in project.tags">
+                        <div class="col-md-4 col-centered">
+                            <img class="logo" alt="Vue logo" src="../assets/img/logos/logo1.png">
+                            <br><br>
+                            <p class="ptitle">Status: {{project.status}}</p>
+                            <p class="ptitle">Tags:
+                                <span v-for="tag in project.tags">
                                 <span>{{tag}}  </span>
                             </span>
-                        </p>
+                            </p>
+                        </div>
+
+                        <div class="col-md-8">
+
+                            <h1 class="title display-3">{{project.name}}</h1><br><br>
+
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <p class="ptitle">Organisation:</p>
+                                </div>
+                                <div class="col-md-8">
+                                    <p> {{project.organisation}}</p>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <p class="ptitle">Description:</p>
+                                </div>
+                                <div class="col-md-8">
+                                    <p>{{project.description}}</p>
+                                </div>
+                            </div>
+
+
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <p class="ptitle">Requirements:</p>
+                                </div>
+                                <div class="col-md-8">
+                                    <p>{{project.requirements}}</p>
+                                </div>
+                            </div>
+
+
+                        </div>
                     </div>
 
-                    <div class="col-md-8">
 
-                        <h1 class="title">{{project.name}}</h1><br><br>
-
-                        <div class="row">
-                            <div class="col-md-4">
-                                <p><b>Organisation:</b></p>
-                            </div>
-                            <div class="col-md-8">
-                                <p> {{project.organisation}}</p>
-                            </div>
+                    <div v-if="errors && errors.length">
+                        <div v-for="error of errors">
+                            {{error.message}}
                         </div>
-
-                        <div class="row">
-                            <div class="col-md-4">
-                                <p><b>Description:</b></p>
-                            </div>
-                            <div class="col-md-8">
-                                <p>{{project.description}}</p>
-                            </div>
-                        </div>
-
-
-                        <div class="row">
-                            <div class="col-md-4">
-                                <p><b>Requirements:</b></p>
-                            </div>
-                            <div class="col-md-8">
-                                <p>{{project.requirements}}</p>
-                            </div>
-                        </div>
-
-
                     </div>
+
+
                 </div>
-
-
-                <div v-if="errors && errors.length">
-                    <div v-for="error of errors">
-                        {{error.message}}
-                    </div>
-                </div>
-
-
+                <button type="button" class="btn btn-danger">CONTRIBUTE</button>
             </div>
         </div>
-    </div>
     </div>
     </body>
 
@@ -111,10 +122,10 @@
         },
         // Fetches projects when the component is created.
         created() {
-            axios.get('http://localhost:5000/contributorapi/project/'+this.$route.params.id+'/')
+            axios.get('http://localhost:5000/contributorapi/project/' + this.$route.params.id + '/')
                 .then(response => {
                     this.projects = response.data;
-                    console.log("resp: "+response.data);
+                    console.log("resp: " + response.data);
                     console.log("proj" + this.projects)
                 })
                 .catch(e => {
@@ -128,6 +139,8 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+    @import url('https://fonts.googleapis.com/css?family=Lato:700');
+
     h3 {
         margin: 40px 0 0;
     }
@@ -147,37 +160,74 @@
     }
 
     .logo {
-        width: 150px;
-        height: 150px;
-        margin-bottom: 50px;
+        width: 12vw;
+        height: auto;
+        padding-top: 1vh;
+        padding-bottom: 2vh;
     }
+
     .container {
         background-color: #fffcf2;
         margin-bottom: 0px;
         height: 100%;
     }
+
     .col-md-8 {
         padding: 30px;
 
     }
+
     .col-md-4 {
         padding: 30px;
 
     }
+
     .title {
         padding: 30px;
     }
+
     html, body {
         background-color: #fffcf2;
         margin: 0px;
         height: 100%;
     }
+
     .container-fluid {
         padding: 0px;
     }
+
     .container {
         margin-top: 40px;
         background-color: white;
-        box-shadow: 6px 7px 17px -6px rgba(0, 0, 0, 0.75)
+        box-shadow: 6px 7px 17px -6px rgba(0, 0, 0, 0.75);
+        border: 1px solid rgba(169, 169, 169, 0.29);
+        padding: 1vh 6vh;
+
+    }
+
+    .btn-danger {
+        width: 15vw;
+        font-weight: bold;
+        margin: 0 auto 5vh;
+        display: block;
+    }
+
+    .ptitle {
+        font-weight: bold;
+        font-size: 2vh;
+
+    }
+
+    .col-centered {
+        display: inline-block;
+        float: none;
+        margin-right: -4px;
+        text-align: center;
+    }
+
+    @media (min-width: 992px) {
+        .navbar {
+            height: 50px;
+        }
     }
 </style>
