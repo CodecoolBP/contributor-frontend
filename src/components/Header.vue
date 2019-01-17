41913a560123d37dbfdbd7f4152293dec942e054
manoczar <mano.czar@gmail.com>
2019-01-17 16:24:41 +0100

    Main page minor fixes

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <div class="container-fluid headercontainer">
+        <div class="container">
+            <div class="img-wrapper">
+                <img class="img-responsive" alt="Header image" src="/Users/manoczar/Downloads/Codecool/Advanced/ContriButor/contributor-frontend/src/assets/titlebackground.png">
+                <div class="img-overlay">
+                    <button class="btn btn-md btn-success btn-responsive" id="addproject">Add new project</button>
+                    <button class="btn btn-md btn-success btn-responsive" id="seeprojects">See active projects
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        data(){
+            return {}
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
