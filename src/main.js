e4f61003cfc2ca8695f874f52de9baec7548c621
kinga00099 <kinga00099@gmail.com>
2019-01-17 09:57:31 +0100

    dir structure changed

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './components/css-reset.css'
+import './assets/css/css-reset.css'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
