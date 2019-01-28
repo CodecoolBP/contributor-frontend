ec8719d61e9a79c4f8f56e0001fdafa60f69b388
kinga00099 <kinga00099@gmail.com>
2019-01-28 12:48:31 +0100

    header component created

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,25 +1,17 @@
 <template>
 
-    <div class="container-fluid headercontainer">
-        <div class="container">
-            <div class="img-wrapper">
-                <img class="img-responsive" alt="Header image" src="/Users/manoczar/Downloads/Codecool/Advanced/ContriButor/contributor-frontend/src/assets/titlebackground.png">
-                <div class="img-overlay">
-                    <button class="btn btn-md btn-success btn-responsive" id="addproject">Add new project</button>
-                    <button class="btn btn-md btn-success btn-responsive" id="seeprojects">See active projects
-                    </button>
-                </div>
-            </div>
+    <div class="img-wrapper">
+        <img class="img-responsive" alt="Header image" src="../assets/img/titlebackground.png">
+        <div class="img-overlay">
+            <a class="btn btn-md btn-warning btn-responsive" href="http://localhost:8080/#/add"
+               role="button" id="addproject">Add new project</a>
+            <button class="btn btn-md btn-warning btn-responsive" id="seeprojects">See active projects
+            </button>
         </div>
     </div>
 </template>
 
 <script>
-    module.exports = {
-        data(){
-            return {}
-        }
-    }
 </script>
 
 <style scoped>
