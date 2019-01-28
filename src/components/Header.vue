c8dcbd6969c17bfbefb9b74858f21c700c7097e8
kinga00099 <kinga00099@gmail.com>
2019-01-28 14:47:04 +0100

    optimize css props

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
     <div class="img-wrapper">
         <img class="img-responsive" alt="Header image" src="../assets/img/titlebackground.png">
         <div class="img-overlay">
-            <a class="btn btn-md btn-warning btn-responsive" href="http://localhost:8080/#/add"
+            <a class="btn btn-md btn-warning " href="http://localhost:8080/#/add"
                role="button" id="addproject">Add new project</a>
             <button class="btn btn-md btn-warning btn-responsive" id="seeprojects">See active projects
             </button>
@@ -41,14 +41,9 @@
         height: 50%;
     }
 
-    .btn-responsive {
-
-    }
-
     #addproject, #seeprojects {
         font-size: 1.5rem;
-        border-style: solid;
-        border-width: 1px;
+        border: 1px solid;
     }
 
     #addproject {
