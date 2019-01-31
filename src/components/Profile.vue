636e8e78d4507a06bf9aeb3e109acc0bf1e2d81c
kinga00099 <kinga00099@gmail.com>
2019-01-31 10:25:16 +0100

    profile page container prepared

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,14 +2,18 @@
     <div>
         <div class="container-fluid">
             <navbar></navbar>
+            <div class="container">
+                <div class="row d-flex align-items-stretch"">
+                    <div class="col-md-3 profiles leftProfile col-centered">
+                        <img class="img-fluid" id="profileImg" src="../assets/img/user.png"
+                             alt="Profile image">
+                        <p>Logged in as: </p>
 
-
-
+                    </div>
+                    <div class="col-md-9 profiles rightProfile">Hello</div>
+                </div>
+            </div>
         </div>
-
-
-
-
 
 
     </div>
@@ -27,5 +31,28 @@
 </script>
 
 <style scoped>
+    .container-fluid {
+        padding: 0;
+    }
 
+    .leftProfile {
+        background-color: #343a40;
+
+    }
+    .container {
+        height: 85vh;
+        overflow: auto;
+        margin-top: 4vh;
+        margin-bottom: 4vh;
+        box-shadow: 6px 7px 17px -6px rgba(0, 0, 0, 0.75);
+        border: 1px solid rgba(169, 169, 169, 0.29);
+
+    }
+    .row{
+        height: 100%;
+    }
+
+    .profiles{
+       padding: 3vh;
+    }
 </style>
